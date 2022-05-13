3ff13213a3b2d9cfe989c50bc47e34fad3403b1b
“Evan <frommere@uchicago.edu>
2022-05-13 10:03:54 -0400

    phase 1

---
+++ reddit/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+    before_action: require_logged_out, only: [:new, :create]
+    before_action: require_logged_in, only: [:index, :show]
+    def new
+        @user = User.new
+        render :new
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @users.errors.full_messages
+            render :new
+        end
+    end
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+    def index
+        @users = User.all
+        render :index
+    end
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
